6845f472bcee3770bd78f6e6b5a29f082551147c
Adrian Gutierrez <ir.da21@outlook.com>
2023-06-07 17:45:52 -0600

    Algunos cambios

---
+++ intro-js/script.js
@@ -0,0 +1,15 @@
+ // Creacion de primero console.log, imprimir en consola
+ console.log("Hola mundo Andres")
+ console.log(5+5*3)
+
+ //Escribe en el documento html
+ document.write("<h1>Hola Mundo Fabio</h1>")
+
+ //Alerta sobre la pantalla
+ alert("HAS GANADO UN IPHONE 15! DA CLIC AQUI PARA SABER MAS")
+
+ //CONSOLE LOG CON LECTURA DE DATOS
+ console.log("Hola Mundo", prompt("como te llamas"));
+
+ //Este es un comentario normal, de una linea
+ /* Este comentario sirve para muchas lineas*/
