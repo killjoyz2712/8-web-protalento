ba3e42c30157baed03219dde9064a0edf4cc0a0d
Roberto Romero <roberto@arrenda.mx>
2023-06-26 20:39:26 -0600

    Validaciones

---
+++ intro-cajero/personas.js
@@ -0,0 +1,32 @@
+let cuentas = [
+    { nombre: "Mali", saldo: 200, password: "6545"}, // 0
+    { nombre: "Gera", saldo: 290, password: "ee89"}, // 1
+    { nombre: "Maui", saldo: 67, password: "91pt" } // 2
+];
+
+document.querySelector("#persona").addEventListener("change",leerPersona)
+
+// cuentas[1] <- Gera
+
+function leerPersona(){
+    let indice = parseInt(document.querySelector("#persona").value);
+    // console.log(cuentas[indice]);
+    let nombre = cuentas[indice].nombre;
+    let saldo = cuentas[indice].saldo;
+
+    if(saldo < 100){
+        alert("Tu saldo se esta acabando");
+    }
+
+    document.getElementById("saludo").innerHTML = "Hola, " + nombre;
+
+    document.getElementById("meter-password").style.display = "block"
+
+}
+
+function irAotraPagina(){
+    window.location.href = "exito.html"
+}
+
+// leerPersona();
+
