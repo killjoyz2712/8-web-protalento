b6c9863d9127e92556b3c3552f6736f531278014
Roberto Romero <roberto@arrenda.mx>
2023-08-16 18:11:58 -0600

    .env dotenv

--- modulo-4/apiDEV/controllers/controllers.js
+++ modulo-4/apiDEV/controllers/controllers.js
@@ -1,11 +1,11 @@
 const {Pool} = require('pg');
 
 const conexion = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: '1234',
-    database: 'restaurante',
-    port: 5432
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 });
 
 
--- modulo-4/apiDEV/index.js
+++ modulo-4/apiDEV/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express'); 
 const app = express();
 
@@ -5,6 +7,6 @@ const rutas = require('./routes/routes')
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(rutas)
-app.listen(3000)
-
-console.log('Servidor en puerto 3000');
+app.listen(process.env.PORT)
+console.log('🚀 Bienvenido a '+process.env.APP_NAME);
+console.log('Servidor en puerto '+process.env.PORT);
