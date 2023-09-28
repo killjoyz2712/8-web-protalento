45d040353ddbcf5ad44d23374f26fe4a792bd87e
Roberto Romero <roberto@ulama.com.mx>
2023-09-27 19:03:47 -0600

    sass

--- modulo-6/intro-react/react-primero/src/components/App.jsx
+++ modulo-6/intro-react/react-primero/src/components/App.jsx
@@ -4,12 +4,21 @@ import Card from './Card'
 
 function App() {
   // const [count, setCount] = useState(0)
-  const hola = "Hola mundo"
-  const hola2 = import.meta.env.VITE_APP_NAME
+  // const hola = "Hola mundo"
+  const hola = import.meta.env.VITE_APP_NAME
+  const description = "Este es un ejemplo de una webapp con react y sass"
   return (
     <div>
       <Navbar/>
-      <h1>{hola}</h1>
+      <div className='main-block' id='main'>
+        <div id='intermedio'>
+          <h1 className='title'>{hola}</h1>
+          <p className='subtitulo'>{description}</p>
+          <p>Dale click al siguiente boton para comenzar</p>
+          <button className='button is-success'>Comenzar mi curso</button>
+        </div>
+      </div>
+      <h1>Prueba</h1>
       <div className="columns">
         <Card/>
         <Card/>
